81f986c905f3e400856a8d7eba73d227d655bcfa
Ikuo Degawa <degawa@ikuwow.com>
2014-11-11 22:32:15 +0900

    initial commit

---
+++ DISampleApp/DITabbarController.swift
@@ -0,0 +1,13 @@
+//
+//  DITabbarController.swift
+//  DISampleApp
+//
+//  Created by 出川 幾夫 on 11/11/14.
+//  Copyright (c) 2014 Ikuo Degawa. All rights reserved.
+//
+
+import UIKit
+
+class DITabbarController: UIViewController {
+    
+}
